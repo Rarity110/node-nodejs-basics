368646f86c94f630e15d541072a89dbc38ed949b
Rarity110 <rarity110@mail.ru>
2022-12-04 21:55:56 +0300

    feat: add zip/compress-decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const compress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filePathTXT = path.join(__dirname, '/files', 'fileToCompress.txt');
+    const filePathGZ = path.join(__dirname, '/files', 'archive.gz');
+   
+    const gzip = createGzip();
+    const txt = createReadStream(filePathTXT);
+    var zip = createWriteStream(filePathGZ);
+    txt.pipe(gzip).pipe(zip);
 };
 
 await compress();
