8598cd55d2b08d3c0b10ea44a2f8aadf22bb0ab7
Rarity110 <rarity110@mail.ru>
2022-12-04 23:29:43 +0300

    feat: add cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,25 @@
+import { spawn } from 'node:child_process';
+import { fileURLToPath } from 'node:url';
+import path from 'path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filePath = path.join(__dirname, 'files', 'script.js');
+
+    const ls = spawn('node', [filePath, ...args]);
+
+
+    process.stdin.pipe(ls.stdin);
+
+	ls.stdout.on('data', (data)=>{
+		console.log(data.toString());
+	})
+
+    ls.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
 };
 
-spawnChildProcess();
+spawnChildProcess(['a', 'b', 'c']);
